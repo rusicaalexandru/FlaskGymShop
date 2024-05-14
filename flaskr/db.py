3a8ea48dcbbdf49e27cde61a42908cb0e480d45a
RusicaAlexandru <aalexandruslan@gmail.com>
2024-05-14 22:26:21 +0300

    DB create tables and modified the user table

--- flaskr/db.py
+++ flaskr/db.py
@@ -11,7 +11,6 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-
     return g.db
 
 
@@ -20,3 +19,16 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
