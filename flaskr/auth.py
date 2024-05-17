d5e6dd4fd9c84f41c4ec6078ed06209f1962d4ed
RusicaAlexandru <aalexandruslan@gmail.com>
2024-05-17 16:01:47 +0300

    Require Authentication in Other Views

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -89,3 +89,10 @@ def logout():
     return redirect(url_for('index'))
 
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view()
