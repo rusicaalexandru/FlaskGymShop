fb31f3cb0c331a887a5801612a30019b23c73ca5
RusicaAlexandru <aalexandruslan@gmail.com>
2024-05-22 21:58:38 +0300

    Basic website functionality
    + Added to the schema dummy data for posts
    + Show / Load posts from db
    + Manually added images for the items
    + CSS style
    + Html for index.htm/post_details.html

--- flaskr/shop.py
+++ flaskr/shop.py
@@ -1,8 +1,29 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, flash
+from flaskr.db import get_db
 bp = Blueprint('shop', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('shop/index.html')
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, p.title, p.description, p.price, p.image_path, p.created, u.username as author'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY p.created DESC'
+    ).fetchall()
+    return render_template('shop/index.html', posts=posts)
+
+
+@bp.route('/post/<int:post_id>')
+def post_detail(post_id):
+    db = get_db()
+    post = db.execute(
+        'SELECT p.id, p.title, p.description, p.price, p.image_path, p.created, u.username as author'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?', (post_id,)
+        ).fetchone()
+
+    if post is None:
+        flash(f"404. Post id {post_id} doesn't exist")
+
+    return render_template('shop/post_detail.html', post=post)
