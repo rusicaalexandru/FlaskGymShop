7388fde149c129dfe8fa118c122c11042a242f37
RusicaAlexandru <aalexandruslan@gmail.com>
2024-05-24 19:17:19 +0300

    Implemented endpoints for the cart

--- flaskr/shop.py
+++ flaskr/shop.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, g, flash, redirect, url_for
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
@@ -15,6 +15,65 @@ def index():
         ' ORDER BY p.created DESC'
     ).fetchall()
     return render_template('shop/index.html', posts=posts)
+
+
+@bp.route('/add_to_cart/<int:post_id>', methods=['POST'])
+@login_required
+def add_to_cart(post_id):
+    db = get_db()
+    user_id = g.user['id']
+
+    # Check if the item is already in the cart
+    cart_item = db.execute(
+        'SELECT * FROM cart WHERE user_id = ? AND post_id = ?', (user_id, post_id,)
+    ).fetchone()
+
+    if cart_item:
+        # Update quantity if the item is already in the cart
+        db.execute(
+            'UPDATE cart SET quantity = quantity + 1 WHERE user_id = ? AND post_id = ?',
+            (user_id, post_id,)
+        )
+    else:
+        # Insert new item into the cart
+        db.execute(
+            'INSERT INTO cart (user_id, post_id, quantity) VALUES (?, ?, ?)',
+            (user_id, post_id, 1,)
+        )
+
+    db.commit()
+    flash('Item added to cart.')
+    return redirect(url_for('shop.index'))
+
+
+@bp.route('/cart')
+@login_required
+def view_cart():
+    db = get_db()
+    user_id = g.user['id']
+
+    cart_items = db.execute(
+        'SELECT c.id, c.quantity, p.id as post_id, p.title, p.description, p.price, p.image_path'
+        ' FROM cart c JOIN post p ON c.post_id = p.id'
+        ' WHERE c.user_id = ?', (user_id,)
+    ).fetchall()
+
+    return render_template('shop/cart.html', cart_items=cart_items)
+
+
+@bp.route('/remove_from_cart/<int:cart_id>', methods=['POST'])
+@login_required
+def remove_from_cart(cart_id):
+    db = get_db()
+    user_id = g.user['id']
+
+    db.execute(
+        'DELETE FROM cart WHERE id = ? AND user_id = ?', (cart_id, user_id)
+    )
+
+    db.commit()
+    flash('Item removed from cart.')
+    return redirect(url_for('shop.view_cart'))
 
 
 @bp.route('/post/<int:post_id>')
